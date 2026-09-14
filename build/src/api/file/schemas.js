24a41c15c9af179eef2858a8627318543c10ca84
zenith979 <zenith@visibleone.com>
2026-09-14 14:53:17 +0700

    feat: build files

--- build/src/api/file/schemas.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.fileUrlQuerySchema = exports.fileUrlBodySchema = void 0;
-const type_provider_typebox_1 = require("@fastify/type-provider-typebox");
-exports.fileUrlBodySchema = type_provider_typebox_1.Type.Object({
-    url: type_provider_typebox_1.Type.String({ minLength: 1, maxLength: 2048 }),
-});
-exports.fileUrlQuerySchema = exports.fileUrlBodySchema;
